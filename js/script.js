376c0a14ae6fd7f3d1638b64cd7a53b98f41e327
TIC97 <thomysurb1997@gmail.com>
2022-10-28 21:20:17 -0300

    CAMBIOS STORAGE

--- js/script.js
+++ js/script.js
@@ -1,96 +1,6 @@
-const nombre = document.getElementById('nombre'),
-    apellido = document.getElementById('apellido'),
-    email = document.getElementById('email'),
-    telefono = document.getElementById('telefono'),
-    pais = document.getElementById('pais'),
-    ciudad = document.getElementById('ciudad'),
-    password = document.getElementById('password'),
-    registro = document.getElementById('register');
+const registro = document.getElementById('form-register');
 
-// ***MODIFICAR EL SECTION AGREGANDO ETIQUETAS HTML POR DOM (falta modificarla !!!)
-// const section = document.getElementsByTagName('section')[0];
-const section = document.getElementsByTagName('section')[0];
-
-//TEST DE LAS VARIABLES DECLARADAS
-console.log(nombre)
-console.log(apellido);
-console.log(email);
-console.log(telefono);
-console.log(pais);
-console.log(ciudad);
-console.log(password);
-console.log(registro);
-console.log(section);
-
-// ***DOM (MODIFICO EL INPUT DE REGISTRO)
-registro.value = 'Registro !!!';
-
-// *** MODIFICO LA ETIQUETA SECTION (averigaur si se puede definir lo que lleva dentro el elemento de una mejoor forma)
-
-// let crearEti = document.createElement('input');
-// crearEti.className = "btn activo";
-// crearEti.type = "submit"
-// crearEti.value = "Registrar";
-
-// section.appendChild(crearEti);
-// ***ARRIBA AGREDO UN ELEMENTO UNICO HTML POR DOM
-
-// *** EVENTOS Y LOCASTORAGE !!!
-// *** CLICK EN EL BOTON REGISTRO (ME QUEDA INCORPORAR UN FILTRO QUE ME LLEVE LOS DATOS A MI ARRAY) TENGO QUE TRABAJAR CON LA PROPIEDAD TARGET ?
-
-registro.addEventListener("click", () => {
-    console.log("Click");
-})
-
-registro.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(e.target);
-});
-
-registro.addEventListener('click', () => {
-    localStorage.setItem("Nombre", nombre.value)
-    localStorage.setItem("Apellido", apellido.value)
-    localStorage.setItem("Email", email.value)
-    localStorage.setItem("Telefono", telefono.value)
-    localStorage.setItem("Pais", pais.value)
-    localStorage.setItem("Ciudad", ciudad.value)
-    localStorage.setItem("Contraseña", password.value)
-});
-
-// ***TOMAR TODOS LOS INPUT Y QUE CON UN EVENTO CHANGE CAMBIE DE CAMPO CUANDO PRESIONE TAB (MODIFIQUE EL HTML Y LE AGREGE UNA ETIQUETA   QUE LOS HACE "tabindex")
-
-// *** HACER UNA FUNCION QUE CAPTURE EL EVENTO UNO POR UNO
-
-    
-/* nombre.addEventListener('input', () =>{
-    console.log(nombre.value);
-});
-apellido.addEventListener('input', () =>{
-    console.log(apellido.value);
-});
-email.addEventListener('input', () =>{
-    console.log(email.value);
-});
-telefono.addEventListener('input', () =>{
-    console.log(telefono.value);
-});
-pais.addEventListener('input', () =>{
-    console.log(pais.value);
-});
-ciudad.addEventListener('input', () =>{
-    console.log(ciudad.value);
-});
-password.addEventListener('input', () =>{
-    console.log(password.value);
-}); */
-
-
-// ***FUNCIONES !!!
-// ***FUNCION ID RANDOM 
-
-const identificacionMax = 10000;
-const identificacionMin = 0;
-function numberUser(min, max){
+function numberUser(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 };
 
@@ -99,39 +9,37 @@ function numberUser(min, max){
 const datosUsuarios = [];
 
 // ***FUNCION COSNTRUCTORA !!!
-
-function Formulario (nombre, apellido, email, telefono, pais, ciudad, password) {
-    this.id = numberUser(identificacionMin, identificacionMax),
-    this.nombre = nombre,
-    this.apellido = apellido,
-    this.email = email,
-    this.telefono = telefono,
-    this.pais = pais,
-    this.ciudad = ciudad,
-    this.password = password
+const identificacionMax = 10000;
+const identificacionMin = 0;
+class Formulario {
+    constructor(nombre, apellido, email, telefono, pais, ciudad, password) {
+        this.id = numberUser(identificacionMin, identificacionMax),
+            this.nombre = nombre,
+            this.apellido = apellido,
+            this.email = email,
+            this.telefono = telefono,
+            this.pais = pais,
+            this.ciudad = ciudad,
+            this.password = password;
+    }
 }
 
-const nuevoUsuario = new Formulario(nombre, apellido, email, telefono, pais, ciudad, password)
-
-// console.log(nuevoUsuario);
-
-// ***FUNCION DE PUSH AL ARRAY USUARIOS !!!
-
-function cargarUsuario (arr, valor) {
-    arr.push(valor);
-};
-cargarUsuario(datosUsuarios, nuevoUsuario);
-
-console.log(datosUsuarios);
-
- 
-
-// // ***FOR OF ? PARA RECORRER EL ARRAY, FILTER PARA DEVOLVER ID DE USER A TRAVEZ DE UN MAP
-//  (***MEJORAR EL FILTRO ***)
-
-// const cantidadUsers = datosUsuarios.map((el) => { return el.id})
-
-// console.log(cantidadUsers);
 
 
+registro.addEventListener('submit', (e) => {
 
+    e.preventDefault();
+    console.log(e.target);
+    const nombre = document.getElementById('nombre').value,
+        apellido = document.getElementById('apellido').value,
+        email = document.getElementById('email').value,
+        telefono = document.getElementById('telefono').value,
+        pais = document.getElementById('pais').value,
+        ciudad = document.getElementById('ciudad').value,
+        password = document.getElementById('password').value
+    const nuevoUsuario = new Formulario(nombre, apellido, email, telefono, pais, ciudad, password)
+    datosUsuarios.push(nuevoUsuario)
+    console.log(nuevoUsuario)
+    localStorage.setItem("usuario", JSON.stringify(nuevoUsuario))
+
+})
